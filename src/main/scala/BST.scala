16887078dbdc9fd9bab9f4db3e5a6e38c05090bc
philhannant <philip.a.hannant@gmail.com>
2018-04-21 21:46:41 +0100

    insert and inorder working

--- src/main/scala/BST.scala
+++ src/main/scala/BST.scala
@@ -8,11 +8,11 @@ object BST {
 
   def insert(v: Int, tree: BST): BST = {
     tree match {
-      case Empty() => Node(Empty(), v, Empty())
       case Node(left, value, right) =>
-        if(v > value) Node(left, value, insert(v, right))
+        if (v > value) Node(left, value, insert(v, right))
         else if (v < value) Node(insert(v, left), value, right)
         else tree
+      case Empty() => Node(Empty(), v, Empty())
     }
   }
 
@@ -20,10 +20,37 @@ object BST {
     tree match {
       case Empty() => false
       case (Node(l, v, r)) =>
-        if(x == v) true
+        if (x == v) true
         else if (x < v) find(x, l)
         else find(x, r)
     }
   }
 
+  def printInorder(tree: BST): Unit = {
+    tree match {
+      case Node(l, v, r) =>
+        printInorder(l)
+        println(v)
+        printInorder(r)
+      case Node(Empty(), v, Empty()) => print(v)
+      case Empty() => print(" ")
+    }
+  }
 }
+
+object Tester extends App {
+  var t = BST.insert(3, Empty())
+  t = BST.insert(10, t)
+  t = BST.insert(72, t)
+  t = BST.insert(21, t)
+  t = BST.insert(18, t)
+  t = BST.insert(4, t)
+  t = BST.insert(3, t)
+  t = BST.insert(1, t)
+
+  BST.printInorder(t)
+  println(BST.find(1, t))
+
+}
+
+
